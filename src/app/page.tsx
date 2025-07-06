d91114b3d9800fc847cfdd19eef5bed8a999920a
Abba Dalhatu <abbadalhat101@gmail.com>
2025-07-06 01:32:06 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+// import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { motion } from "framer-motion";
 import {
